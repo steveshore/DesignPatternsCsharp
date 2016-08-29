19807bfae72dce0dfe69418bd290e24f8c5a78a7
Steven.Shore <Steven.Shore@e-hps.com>
2016-08-28 20:00:17 -0400

    Adapter Pattern

---
+++ 03-Adapter/AdapterPattern/AdapterPattern/MealServiceOne.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+    //Get meal via string
+    public class MealServiceOne
+    {
+        public string GetMeal()
+        {
+            Console.Write("What meal would you like? (Breakfast, Lunch, or Dinner):\n");
+            var meal = Console.ReadLine();
+            if (meal != null)
+            {
+                switch (meal.ToLower())
+                {
+                    case "breakfast":
+                    case "lunch":
+                    case "dinner":
+                        return meal;
+                }
+
+            }
+
+            Console.Write("That's not a meal. Try again...\n\n");
+            return GetMeal();
+        }
+
+    }
+}
---
+++ 03-Adapter/AdapterPattern/AdapterPattern/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = ChooseServiceType();
+            if (service == "string")
+            {
+                var mealOne = new MealServiceOne().GetMeal();
+                Console.Write("you chose: " + mealOne);
+                Console.ReadKey();
+            }
+            if (service == "int")
+            {
+                var mealTwo = new MealServiceTwo().GetMeal();
+                Console.Write("you chose: " + mealTwo);
+                Console.ReadKey();
+            }
+        }
+        //decides which service to use 
+        public static string ChooseServiceType()
+        {
+            Console.Write("Would you like to choose by string or int:\n");
+            var serviceType = Console.ReadLine();
+            if (serviceType != null)
+            {
+                if (serviceType.ToLower() == "string" || serviceType.ToLower() == "int")
+                {
+                    return serviceType.ToLower();
+                }
+            }
+            Console.Write("Type better. Enter string or int:");
+            return ChooseServiceType();
+        }
+    }
+}
